0621d882d076b62bc9ed0f047b04642eaabc955f
Matt Thurling <mthurling@icloud.com>
2022-09-14 13:19:18 +0100

    smtpjs

---
+++ js/smtp-jquery.js
@@ -0,0 +1,11 @@
+(function ( $ ) {
+ 
+  $.getScript( "https://smtpjs.com/v2/smtp.js", function( data, textStatus, jqxhr ) {
+      $.fn.Email = Email;	
+  });
+
+  $.fn.Email = function() {
+      return "Not Loaded yet";
+  };
+
+}( jQuery ));
